77e4e0c8ae630caa70a484271fba2ec2f605d989
Fatemeh-Amiri <ftemeh.amiirii@gmail.com>
2023-07-31 23:05:23 +0330

    1

--- Todo-list-app/main.py
+++ Todo-list-app/main.py
@@ -1,10 +1,16 @@
-user_prompt = "Enter a todo: "
+# user_prompt = "Enter a todo: "
 
-todos = []
+# todos = []
 
-while True:
-    todo = input(user_prompt)
-    todos.append(todo)
-    print(todos)
+# while True:
+#     todo = input(user_prompt)
+#     todos.append(todo)
+#     print(todos)
 
 
+password = input("Enter password:")
+
+while password != "pass123":
+    password = input("Enter password:")
+
+print("Password wac correct")
