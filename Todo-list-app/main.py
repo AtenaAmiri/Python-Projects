84de8185421068462084cab9583df5cbecf9a706
Fatemeh-Amiri <ftemeh.amiirii@gmail.com>
2023-08-08 18:18:24 +0330

    add close and open function

--- Todo-list-app/main.py
+++ Todo-list-app/main.py
@@ -1,22 +1,31 @@
-todos = []
 while True:
-    user_action = input("Type add,show,edit,complete or exit: ")
+    user_action = input("Type add,show,edit,delete or exit: ")
     user_action = user_action.strip()
     match user_action:
         case "add":
-            todo = input("Enter a todo : ")
+            todo = input("Enter a todo : ") + "\n"
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open("todos.txt", "w")
+            file.writelines(todos)
+            file.close()
+
         case "show":
             for index, item in enumerate(todos):
                 item = item.title()
-                print(f"{index + 1} - {item}")
+                row = f"{index + 1} - {item}"
+                print(row)
         case "edit":
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
-        case "complete":
-            number = int(input("Number of the todo to complete: "))
+        case "delete":
+            number = int(input("Number of the todo to delete: "))
             todos.pop(number - 1)
         case "exit":
             break
