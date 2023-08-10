029468a408fbe52e747676040aac0820a2c9958c
Fatemeh-Amiri <ftemeh.amiirii@gmail.com>
2023-08-10 13:25:36 +0330

    make app more userFriendly

--- Todo-list-app/main.py
+++ Todo-list-app/main.py
@@ -25,10 +25,10 @@ while True:
             #     new_item = item.strip("\n")
             #     new_todos.append(new_item)
 
-            new_todos = [item.strip("\n") for item in todos]
+            # new_todos = [item.strip("\n") for item in todos]
 
-            for index, item in enumerate(new_todos):
-                item = item.title()
+            for index, item in enumerate(todos):
+                item = item.title().strip("\n")
                 row = f"{index + 1} - {item}"
                 print(row)
 
